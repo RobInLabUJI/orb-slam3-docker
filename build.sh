77b2a57f07c769318327d98bf60129be479aac66
Enric Cervera <ecervera@uji.es>
2022-07-07 09:02:40 +0200

    First version

---
+++ build.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+sudo docker build -t robinlab/orb-slam3 .
---
+++ run.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+sudo docker run --privileged --rm -e DISPLAY=$DISPLAY -e QT_X11_NO_MITSHM=1 -v /tmp/.X11-unix:/tmp/.X11-unix -v /dev:/dev:ro -it robinlab/orb-slam3
+
